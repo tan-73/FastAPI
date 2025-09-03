c85adc8a84072ba95a5d88acbf2baab024594c0b
Tanmay <anandtanmay474@gmail.com>
2025-09-03 18:56:14 +0530

    Add JWT practice file

--- practice.py
+++ practice.py
@@ -68,3 +68,5 @@ async def admin_route(current_user: dict = Depends(get_current_user)):
     if current_user.get("role") != "admin":
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Access forbidden")
     return {"message": "Welcome, admin!"}
+
+#practice
